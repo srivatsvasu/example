a53e837d81a5b1560ae40beac04e491cc185f063
srivatsan <srivats.vasu@gmail.com>
2018-06-04 12:14:12 -0500

    add changes

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		w.Write([]byte("Hello World"))
 
 	})
 	http.ListenAndServe(":8000", nil)
